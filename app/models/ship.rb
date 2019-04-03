070e759169cfd8e3498b84a8f09d2b88ce4e6fa3
bebopvinh <vinhdo07@gmail.com>
2019-04-02 22:40:13 -0700

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,19 @@
 class Ship
+   attr_accessor :name, :type, :booty
+   ALL = []
+
+   def initialize(hash)
+      hash.each do |k,v|
+         instance_variable_set("@#{k}", v) unless v.nil?
+      end
+      ALL << self
+   end
+
+   def self.all
+      ALL
+   end
+
+   def self.clear
+      ALL.clear
+   end
 end
